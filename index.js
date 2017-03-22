c219580a62b346dd6a6804e4e2b5c854f4732b08
adumbidiot <nathaniel.daniel23@outlook.com>
2017-03-22 14:38:17 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use('/', function(req, res, next){
+	console.log(req.path);
+	next();
+});
+
+app.get('/', function(req, res){
+	res.send('HELLO WORLD!');
+});
+
+app.listen('80');
